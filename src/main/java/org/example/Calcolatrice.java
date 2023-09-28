f20af50d0d8470cb988786e756c6b463e39a7b81
Giovanni <giovanni.f.liguori@gmail.com>
2023-09-28 23:30:15 +0200

    Aggiunti commenti Javadoc ai metodi somma() e moltiplicazione() di Calcolatrice.java

--- src/main/java/org/example/Calcolatrice.java
+++ src/main/java/org/example/Calcolatrice.java
@@ -4,11 +4,23 @@ public class Calcolatrice
 {
 	// private int a, b;
 	
+	// public Calcolatrice() {}
+	
+	/**Calcola la somma fra due numeri
+	 * @param a primo addendo
+	 * @param b secondo addendo
+	 * @return il risultato dell'addizione
+	 * */
 	public int somma(int a, int b)
 	{
 		return a+b;
 	}
 	
+	/**Calcola il prodotto di due numeri
+	 * @param a il moltiplicando
+	 * @param b il moltiplicatore
+	 * @return il prodotto della moltiplicazione
+	 * */
 	public int moltiplicazione(int a, int b)
 	{
 		return a*b;
