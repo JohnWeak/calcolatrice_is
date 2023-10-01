13145965286ff9399ac87e10f76dd70740263d94
Giovanni <giovanni.f.liguori@gmail.com>
2023-10-01 20:00:42 +0200

    Aggiunte le funzionalità di sottrazione e divisione

--- src/main/java/org/example/Calcolatrice.java
+++ src/main/java/org/example/Calcolatrice.java
@@ -26,4 +26,27 @@ public class Calcolatrice
 		return a*b;
 	}
 	
+	/**Calcola la differenza di due numeri
+	 * @param a il minuendo
+	 * @param b il sottraendo
+	 * @return il risultato della sottrazione
+	 * */
+	public int sottrazione(int a, int b)
+	{
+		return a-b;
+	}
+	
+	/**Calcola la divisione di due numeri sotto forma di double
+	 * @param a il dividendo
+	 * @param b il divisore
+	 * @return il risultato della divisione; -1 se il divisore è zero*/
+	public double divisione(int a, int b)
+	{
+		if (b != 0)
+			return (double) a/b;
+		else
+			return -1;
+	}
+	
+	
 } // fine classe Calcolatrice
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,9 +6,13 @@ public class Main
 	{
 		Calcolatrice calcolatrice = new Calcolatrice();
 		int somma = calcolatrice.somma(1,2);
-		int moltiplicazione = calcolatrice.moltiplicazione(19, 2);
+		int moltiplicazione = calcolatrice.moltiplicazione(4, 7);
+		int sottrazione = calcolatrice.sottrazione(3,10);
+		double divisione = calcolatrice.divisione(5,2);
 		
-		System.out.printf("Somma = %d\nMoltiplicazione = %d\n\n", somma, moltiplicazione);
+		String resultString = String.format("Somma = %d\nMoltiplicazione = %d\nSottrazione = %d\nDivisione = %.1f\n\n", somma, moltiplicazione, sottrazione, divisione);
+		
+		System.out.printf(resultString);
 		
 	} // fine main()
 	
